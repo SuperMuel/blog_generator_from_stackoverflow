4f15dbdf10375ef0cf4c30c446d4d22cf7c12ff4
SuperMuel <69467005+SuperMuel@users.noreply.github.com>
2024-05-29 11:49:09 +0200

    Prepare for Heroku deployment

--- api.py
+++ api.py
@@ -32,4 +32,7 @@ def generate_article_endpoint():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    from os import environ
+
+    port = int(environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
